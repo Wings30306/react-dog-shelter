19d852d27303b2cc1ab9630b068f55e2ec602e2c
Joke Heyndels <jo_hannah@outlook.com>
2021-06-02 09:49:51 +0000

    Set up images and dog data

--- shelter/src/App.js
+++ shelter/src/App.js
@@ -1,12 +1,53 @@
+import { Component } from 'react';
 import './App.css';
+import whiskey from "./images/whiskey.jpg"
+import hazel from "./images/hazel.jpg"
+import tubby from "./images/tubby.jpg"
 
-function App() {
-  return (
-    <div className="App">
-
-      <h1 className="display-1">Dog App!</h1>
-    </div>
-  );
+class App extends Component {
+  defaultProps = {
+    dogs: [
+      {
+        name: "Whiskey",
+        age: 5,
+        src: whiskey,
+        facts: [
+          "Whiskey loves eating popcorn.",
+          "Whiskey is a terrible guard dog.",
+          "Whiskey wants to cuddle with you!"
+        ]
+      },
+      {
+        name: "Hazel",
+        age: 3,
+        src: hazel,
+        facts: [
+          "Hazel has soooo much energy!",
+          "Hazel is highly intelligent.",
+          "Hazel loves people more than dogs."
+        ]
+      },
+      {
+        name: "Tubby",
+        age: 4,
+        src: tubby,
+        facts: [
+          "Tubby is not the brightest dog",
+          "Tubby does not like walks or exercise.",
+          "Tubby loves eating food."
+        ]
+      }
+    ]
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1 className="display-1">Dog App!</h1>
+        
+      </div>
+    );
+  }
+  
 }
 
 export default App;
