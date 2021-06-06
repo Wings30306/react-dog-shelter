061d1de37cb9d03e2079ecf3eeaddb16c4b181d6
Joke Heyndels <jo_hannah@outlook.com>
2021-06-06 09:02:11 +0000

    Make DogList responsive

--- shelter/src/DogList.js
+++ shelter/src/DogList.js
@@ -1,15 +1,17 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
+import "./DogList.css"
 
 
 class DogList extends Component {
     render() {
         return (
-            <div className="container">
+            <div className="container DogList">
                 <div className="row">
                     {this.props.dogs.map(dog => (
-                        <Link className="col-4" exact to={`/${dog.name.toLowerCase()}`}>
-                            <img src={dog.src} alt={dog.name} width={"100%"} height="auto"/>
+                        <Link className="col-12 col-md-6 col-lg-4" exact to={`/${dog.name.toLowerCase()}`}>
+                            <img className="rounded-circle img-thumbnail" src={dog.src} alt={dog.name}/>
+                            <h2>{dog.name}</h2>
                         </Link>
                     ))}
                 </div>
