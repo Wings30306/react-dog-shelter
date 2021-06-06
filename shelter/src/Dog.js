2bf21cc5b1b5f3a5fd741f81c2b2193a7e1b1200
Joke Heyndels <jo_hannah@outlook.com>
2021-06-06 10:01:21 +0000

    Style the Dog component

--- shelter/src/Dog.js
+++ shelter/src/Dog.js
@@ -1,20 +1,30 @@
 import { Component } from "react";
+import "./Dog.css"
 
 
 class Dog extends Component {
     render() {
-        let { name, src, facts } = this.props.dog
+        let { name, age, src, facts } = this.props.dog
         return (
-            <div className="container">
+            <div className="Dog container">
                 <div className="row">
-                    <div className="col-6">
-                        <img src={src} alt={name} width={"100%"} height="auto"/>
-                    </div>
-                    <div className="col-6">
-                        <h1>{name}</h1>
-                        <ul>
-                            {facts.map(fact => <li>{fact}</li>)}
-                        </ul>
+                    <div className="card">
+                        <div className="card-image">
+                            <img className="img-thumbnail" src={src} alt={name} width={"100%"} height="auto" />
+                        </div>
+                        <div className="align-left card-body">
+                            <div className="card-body">
+                                <h1 className="card-title">
+                                    {name}
+                                </h1>
+                                <h4 className="card-subtitle tex-muted">
+                                    {age} years old
+                                </h4>
+                                <ul className="list-group list-group-flush">
+                                    {facts.map((fact, i) => <li key={i} className="list-group-item">{fact}</li>)}
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
