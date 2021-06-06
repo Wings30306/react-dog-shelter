d2f6c4210578bd27befc266fa16ca1fae1608cc1
Joke Heyndels <jo_hannah@outlook.com>
2021-06-06 10:16:38 +0000

    Change class (copied from Bootstrap) to className where required

--- shelter/src/Nav.js
+++ shelter/src/Nav.js
@@ -5,15 +5,15 @@ import { NavLink } from "react-router-dom"
 class Nav extends Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <div class="container-fluid">
-                    <NavLink class="navbar-brand" exact to="/">DogShelter</NavLink>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <NavLink className="navbar-brand" exact to="/">DogShelter</NavLink>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            {this.props.dogs.map(dog => <NavLink className="nav-link" exact to={`/${dog.name}`}>{dog.name}</NavLink>)}
+                    <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div className="navbar-nav">
+                            {this.props.dogs.map(dog => <NavLink key={dog.name} className="nav-link" exact to={`/${dog.name}`}>{dog.name}</NavLink>)}
                         </div>
                     </div>
                 </div>
